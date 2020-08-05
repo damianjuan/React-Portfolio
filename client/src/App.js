8c009024aea6021b7a65ac56ef11fef16f2c46ab
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-05 14:54:34 -0500

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TailWindTest from './pages/TailWindTest';
-import NoMatch from './pages/NoMatch';
-import TestPage2 from './pages/TestPage2';
 
 function App() {
   return (
-    <Router>
-      <div>
-
-        <Switch>
-          <Route exact path={"/"}>
-            <TailWindTest />
-          </Route>
-          <Route exact path="/page2">
-            <TestPage2 />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './tailwind.css'
 
 ReactDOM.render(
   <React.StrictMode>

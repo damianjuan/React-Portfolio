a03433376226847ac3988c2a1b9ae8a205b722f2
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-07 21:18:51 -0500

    Updates

---
+++ precache-manifest.16cd5abe8b37cd9d96b795bb7c57e208.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16d97be07ad926d81be05e85f57999a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "a1c62c1acc181787ae96",
+    "url": "/React-Portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fb9057e0f66dde311ae",
+    "url": "/React-Portfolio/static/js/2.6a4eac4d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Portfolio/static/js/2.6a4eac4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c62c1acc181787ae96",
+    "url": "/React-Portfolio/static/js/main.68f8bdb5.chunk.js"
+  },
+  {
+    "revision": "69cfd8c03a1dcd4ed94e",
+    "url": "/React-Portfolio/static/js/runtime-main.2850e5db.js"
+  },
+  {
+    "revision": "64a3700e0358dc41f57d8bbe526742e6",
+    "url": "/React-Portfolio/static/media/employee_dir.64a3700e.jpg"
+  },
+  {
+    "revision": "5b025409527ced2ca0fb1cb04a1d4ef3",
+    "url": "/React-Portfolio/static/media/me.5b025409.jpg"
+  },
+  {
+    "revision": "eea43d92c89ffdf72316b2ee79e1deaa",
+    "url": "/React-Portfolio/static/media/project_boolean_screen_shot.eea43d92.jpg"
+  },
+  {
+    "revision": "0173b051bc8379908c24cdf710d08b21",
+    "url": "/React-Portfolio/static/media/readme_screen_shot.0173b051.jpg"
+  },
+  {
+    "revision": "37dfdadbdd849e32f267619c19090164",
+    "url": "/React-Portfolio/static/media/weather_dashboard_screen_shot.37dfdadb.jpg"
+  }
+]);

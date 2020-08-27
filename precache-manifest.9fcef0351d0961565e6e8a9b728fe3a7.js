58755d22d167796115605674f03aea9f4e12dc83
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-27 13:41:11 -0500

    Updates

---
+++ precache-manifest.9fcef0351d0961565e6e8a9b728fe3a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a233dd1113d42858402cef6e2b7c6f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d41f360aefdac347e33d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1fb9057e0f66dde311ae",
+    "url": "/static/js/2.6a4eac4d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6a4eac4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41f360aefdac347e33d",
+    "url": "/static/js/main.a67ff13d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "64a3700e0358dc41f57d8bbe526742e6",
+    "url": "/static/media/employee_dir.64a3700e.jpg"
+  },
+  {
+    "revision": "5b025409527ced2ca0fb1cb04a1d4ef3",
+    "url": "/static/media/me.5b025409.jpg"
+  },
+  {
+    "revision": "eea43d92c89ffdf72316b2ee79e1deaa",
+    "url": "/static/media/project_boolean_screen_shot.eea43d92.jpg"
+  },
+  {
+    "revision": "0173b051bc8379908c24cdf710d08b21",
+    "url": "/static/media/readme_screen_shot.0173b051.jpg"
+  },
+  {
+    "revision": "37dfdadbdd849e32f267619c19090164",
+    "url": "/static/media/weather_dashboard_screen_shot.37dfdadb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Portfolio/precache-manifest.16cd5abe8b37cd9d96b795bb7c57e208.js"
+  "/precache-manifest.9fcef0351d0961565e6e8a9b728fe3a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
